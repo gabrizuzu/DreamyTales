9eef80b26f41676be602a188e3ce4733db2fac7f
gabrizuzu <zuzugabriele24@gmail.com>
2023-12-11 18:14:26 +0100

    autentificazione funzionante

---
+++ android/app/src/main/kotlin/com/example/dreamy_tales/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dreamy_tales
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,45 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:dreamy_tales/auth.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+  HomePage({Key? key}) : super(key:key);
+  final User? user = Auth().currentUser;
+
+  Future<void> signOut() async {
+    await Auth().signOut();
+  }
+  Widget _title(){
+    return const Text('Dreamy Tales');
+  }
+  Widget _userUid(){
+    return Text(user?.email ?? 'User email');
+  }
+  Widget _signOutButton(){
+    return ElevatedButton(
+        onPressed: signOut,
+        child: const Text('Sign Out'),
+    );
+  }
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: _title(),
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _userUid(),
+            _signOutButton(),
+          ]
+        )
+      )
+    );
+  }
+}
