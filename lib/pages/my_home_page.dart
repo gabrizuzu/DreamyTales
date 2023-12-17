1af75c030275bba0c761b08f8daf182011c5f695
JaskaranRam <jaskaran.ram@mail.polimi.it>
2023-12-17 17:32:12 +0100

    Struttura statica app solida

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -340,7 +340,7 @@ class MyDrawer extends StatelessWidget {
                 icon,
                 color: isSelected ? Colors.white : Colors.deepPurple,
               ),
-              SizedBox(width: 8),
+              SizedBox(width: 16),
               Text(
                 title,
                 style: TextStyle(
--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,17 +1,120 @@
-// settings_screen.dart
 import 'package:flutter/material.dart';
 
+import 'login_register_page.dart';
+
 class SettingsScreen extends StatelessWidget {
-  const SettingsScreen({super.key});
+  const SettingsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Settings'),
-      ),
-      body: Center(
-        child: Text('This is the Settings Screen'),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/sfondo.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Opzione 1: Lingua
+            InkWell(
+              onTap: () {
+                // Implementa la gestione dell'opzione qui
+              },
+              child: ListTile(
+                title: Text('Language'),
+                subtitle: Text('Select the language of the app'),
+                trailing: Icon(Icons.arrow_forward),
+              ),
+            ),
+            Divider(),
+
+            // Opzione 2: Notifiche
+            InkWell(
+              onTap: () {
+                // Implementa la gestione dell'opzione qui
+              },
+              child: ListTile(
+                title: Text('Notifications'),
+                subtitle: Text('Enable or disable app notifications'),
+                trailing: Icon(Icons.arrow_forward),
+              ),
+            ),
+            Divider(),
+
+            // Opzione 3: Tema dell'applicazione
+            ListTile(
+              title: Text('App Theme'),
+              subtitle: Text('Choose between light and dark themes'),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                // Implementa la gestione dell'opzione qui
+              },
+            ),
+            Divider(),
+
+            // Opzione 4: Feedback
+            ListTile(
+              title: Text('Feedback'),
+              subtitle: Text('Send feedback or report issues'),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                // Implementa la gestione dell'opzione qui
+              },
+            ),
+            Divider(),
+
+            // Opzione 5: Informazioni sull'app
+            ListTile(
+              title: Text('App Information'),
+              subtitle: Text('View information about the app'),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                // Implementa la gestione dell'opzione qui
+              },
+            ),
+            Divider(),
+
+            // Opzione 6: Privacy
+            ListTile(
+              title: Text('Privacy'),
+              subtitle: Text('View our privacy policy'),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                // Implementa la gestione dell'opzione qui
+              },
+            ),
+            Divider(),
+
+            // Opzione 7: Esci dall'account
+            ListTile(
+              title: Text('Sign Out'),
+              subtitle: Text('Sign out from your account'),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                );
+              },
+            ),
+            Divider(),
+
+            // Opzione 8: Resetta impostazioni
+            ListTile(
+              title: Text('Reset Settings'),
+              subtitle: Text('Restore default app settings'),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                // Implementa la gestione dell'opzione qui
+              },
+            ),
+            Divider(),
+          ],
+        ),
       ),
     );
   }
