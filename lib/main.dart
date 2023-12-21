d77048a44b23062ba25be3418a900ccfea5ca4ea
JaskaranRam <jaskaran.ram@mail.polimi.it>
2023-12-21 19:15:08 +0100

    Notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,23 @@
 import 'package:dreamy_tales/widget_tree.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    _configureFirebaseMessaging();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
@@ -22,4 +26,26 @@ class MyApp extends StatelessWidget {
       home: const WidgetTree(),
     );
   }
+
+  Future<void> _configureFirebaseMessaging() async {
+    _firebaseMessaging.getToken().then((token) {
+      print("Firebase Token: $token");
+    });
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+      print("onMessage: $message");
+      // Qui puoi gestire la notifica ricevuta mentre l'app è in primo piano
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) async {
+      print("onMessageOpenedApp: $message");
+      // Qui puoi gestire la notifica quando l'app è in background o terminata
+    });
+
+    // Richiedi i permessi per le notifiche
+    NotificationSettings settings = await _firebaseMessaging.requestPermission();
+    print('User granted permission: ${settings.authorizationStatus}');
+  }
+
+
 }
