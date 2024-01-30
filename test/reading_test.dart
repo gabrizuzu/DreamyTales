843f298109252f578926da232070a69228d316d0
gabrizuzu <zuzugabriele24@gmail.com>
2024-01-30 17:20:43 +0100

    modified:   pubspec.lock 	modified:   pubspec.yaml 	modified:   test/reading_test.dart 	new file:   test/signup_test.dart 	modified:   to_do_list

--- test/reading_test.dart
+++ test/reading_test.dart
@@ -1,55 +1,29 @@
-import 'package:dreamy_tales/pages/reading_page.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
 
 void main() {
-  testWidgets('ReadingPage Test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(
-      MaterialApp(
-        home: ReadingPage(
-          storyText: 'Test story text',
-          language: 'Italiano',
-        ),
-      ),
-    );
+  group('My App', () {
+    final readingPageFinder = find.byValueKey('ReadingPage');
 
-    // Verifica che la pagina sia stata costruita correttamente
-    expect(find.text('Reading Page'), findsOneWidget);
-    expect(find.byType(Markdown), findsOneWidget);
-    expect(find.byType(FloatingActionButton), findsNWidgets(2));
-    expect(find.byIcon(Icons.play_arrow), findsOneWidget);
+    FlutterDriver driver;
 
-    // Accedi a _ReadingPageState usando tester.state
-    final state = tester.state<ReadingPageState>(find.byType(ReadingPage));
+    // Connetti il driver prima di eseguire i test
+    setUpAll(() async {
+      driver = await FlutterDriver.connect();
+    });
 
-    // Verifica che il Text-to-Speech (TTS) sia inizializzato correttamente
-    expect(state._flutterTts, isNotNull);
+    // Chiudi il driver dopo l'esecuzione dei test
+    tearDownAll(() async {
+      FlutterDriver driver;
 
-    // Verifica che il TTS sia inizializzato con la lingua corretta
-    expect(
-      state._flutterTts.getLanguage,
-      state.widget.language == 'Italiano' ? 'it-IT' : 'en-US',
-    );
+      if (driver != null) {
+        driver.close();
+      }
+    });
 
-    // Verifica che il Toggle TTS funzioni correttamente
-    await tester.tap(find.byIcon(Icons.play_arrow));
-    await tester.pump();
-    expect(state._isPlaying, isTrue);
-
-    await tester.tap(find.byIcon(Icons.pause));
-    await tester.pump();
-    expect(state._isPlaying, isFalse);
-
-    // Verifica che il Reset TTS funzioni correttamente
-    await tester.tap(find.byIcon(Icons.refresh));
-    await tester.pump();
-    expect(state._isPlaying, isFalse);
-
-    // Verifica che la pagina si chiuda correttamente
-    await tester.pageBack();
-    await tester.pumpAndSettle();
-    expect(state._isPlaying, isFalse);
+    test('check reading page', () async {
+      // Verifica che la ReadingPage sia presente
+      expect(await driver.getText(readingPageFinder), 'ReadingPage');
+    });
   });
 }
