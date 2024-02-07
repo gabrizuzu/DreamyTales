86745d93eddf1b7f34b84781828e81d69f509db4
gabrizuzu <zuzugabriele24@gmail.com>
2024-02-07 16:00:04 +0100

    modified:   lib/pages/plot_page.dart

--- lib/pages/plot_page.dart
+++ lib/pages/plot_page.dart
@@ -1,8 +1,7 @@
 import 'package:dreamy_tales/widgets/moral_page.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:responsive_framework/responsive_framework.dart';
-
+import 'package:flutter_device_type/flutter_device_type.dart';
 class PlotChoice extends StatefulWidget {
   const PlotChoice({super.key});
 
@@ -26,24 +25,105 @@ class _PlotChoiceState extends State<PlotChoice> {
         title: const Text('Classic Plot Choice'),
         backgroundColor: Colors.deepPurple,
       ),
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/sfondo.jpg"),
-            fit: BoxFit.cover,
-          ),
+      body: Device.get().isTablet
+        ? // Layout per iPad
+        Row(
+            children: [
+              Expanded(
+                child: buildGridView(context),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.black.withOpacity(0.6),
+                  child: selectedPlot != null
+                      ? Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Text(
+                                selectedPlot!,
+                                style: const TextStyle(
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.amber,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Text(
+                                plots
+                                    .firstWhere((element) =>
+                                        element['description'] == selectedPlot)
+                                    ['trama']!,
+                                style: const TextStyle(
+                                  fontSize: 15.0,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ],
+                        )
+                      : const Center(
+                          child: Text(
+                            'Select a plot',
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.amber,
+                            ),
+                          ),
+                        ),
+                ),
+              ),
+            ],
+          )
+        : // Layout per iPhone
+        buildGridView(context),
+    floatingActionButton: selectedPlot != null
+        ? FloatingActionButton.extended(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const MoralChoice()),
+              );
+            },
+            label: const Text('Confirm'),
+            backgroundColor: Colors.deepPurple,
+          )
+        : null,
+  );
+}
+
+Widget buildGridView(BuildContext context) {
+  return Container(
+    decoration: const BoxDecoration(
+      image: DecorationImage(
+        image: AssetImage("assets/sfondo.jpg"),
+        fit: BoxFit.cover,
+      ),
+    ),
+    child: Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 1,
+          childAspectRatio: MediaQuery.of(context).orientation == Orientation.portrait
+              ? 1.5
+              : 1,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-        child: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 1,
-            childAspectRatio: ResponsiveWrapper.of(context).isSmallerThan(TABLET) ? 3/2 : 3,
-            crossAxisSpacing: 20,
-            mainAxisSpacing: 20,
-          ),
-          itemCount: plots.length,
-itemBuilder: (context, index) {
+        itemCount: plots.length,
+        itemBuilder: (context, index) {
+          return buildPlotCard(context, index);
+        },
+      ),
+    ),
+  );
+}
+
+Widget buildPlotCard(BuildContext context, int index) {
   return GestureDetector(
     onTap: () async {
       SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -51,6 +131,7 @@ itemBuilder: (context, index) {
       setState(() {
         selectedPlot = plots[index]['description'];
       });
+      // ignore: use_build_context_synchronously
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('You selected ${plots[index]['description']}')),
       );
@@ -59,23 +140,24 @@ itemBuilder: (context, index) {
       Future.delayed(const Duration(milliseconds: 1000), () {
         ScaffoldMessenger.of(context).hideCurrentSnackBar();
       });
-
     },
     child: Column(
       children: [
         Card(
           color: Colors.black.withOpacity(0.6),
           child: Container(
-            height:  MediaQuery.of(context).orientation == Orientation.portrait
-              ? MediaQuery.of(context).size.height * 0.2
-              : MediaQuery.of(context).size.height * 0.35,
+            height: MediaQuery.of(context).orientation == Orientation.portrait
+                ? MediaQuery.of(context).size.height * 0.2
+                : MediaQuery.of(context).size.height * 0.5,
             decoration: BoxDecoration(
               image: DecorationImage(
                 image: AssetImage(plots[index]['image']!),
                 fit: BoxFit.cover,
               ),
               border: Border.all(
-                color: selectedPlot == plots[index]['description'] ? Colors.deepPurple : Colors.transparent,
+                color: selectedPlot == plots[index]['description']
+                    ? Colors.deepPurple
+                    : Colors.transparent,
                 width: 5.0,
               ),
               borderRadius: BorderRadius.circular(10.0),
@@ -83,33 +165,20 @@ itemBuilder: (context, index) {
           ),
         ),
         Padding(
-          padding: const EdgeInsets.only(top:10.0),
+          padding: const EdgeInsets.only(top: 10.0),
           child: Text(
             plots[index]['description']!,
             style: TextStyle(
-              fontSize: 15.0, 
+              fontSize: 15.0,
               fontWeight: FontWeight.bold,
-              color: selectedPlot == plots[index]['description'] ? Colors.deepPurple : Colors.white,
-              ),
+              color: selectedPlot == plots[index]['description']
+                  ? Colors.deepPurple
+                  : Colors.white,
+            ),
           ),
         ),
       ],
     ),
   );
-},
-        ),
-      )
-      ),
-      floatingActionButton: selectedPlot != null ? FloatingActionButton.extended(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const MoralChoice()),
-          );
-        },
-        label: const Text('Confirm'),
-        backgroundColor: Colors.deepPurple,
-      ) : null,
-    );
-  }
+}
 }
